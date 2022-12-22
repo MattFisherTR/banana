6773324ca7c7ca3c16f26a7faae88ca8853ca5cc
Matt <dmitrijdeputatov952@gmail.com>
2022-12-22 15:26:34 +0300

    А в натуральной степени Б

---
+++ work1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("введите число A");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число B");
+int b = int.Parse(Console.ReadLine()!);
+int banan = a;
+int i = 1;
+while (i < b)
+{
+banan = banan * a;
+i++;
+}
+Console.WriteLine("A в степени B равно: " + banan);
